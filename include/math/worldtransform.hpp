563b2d97d1143c4a04dde120b62769baa07856a4
curloz123 <curloz@tutamail.com>
2026-07-03 15:21:35 +0530

    fix: fixed matrix addition bug in mat4x4.hpp

--- include/math/worldtransform.hpp
+++ include/math/worldtransform.hpp
@@ -10,15 +10,19 @@ namespace clz::math
 	inline mat4 getModelMatrix(const quat& r, const vec3& t, const vec3& s)
 	{
 		const mat4 rotation = {
-		    _mm_set_ps(0.0f, 2.0f * (r.x * r.z - r.y * r.w), 2.0f * (r.x * r.y + r.z * r.w), 1.0f - 2.0f * (r.y * r.y + r.z * r.z)),
-		    _mm_set_ps(0.0f, 2.0f * (r.y * r.z + r.x * r.w), 1.0f - 2.0f * (r.x * r.x + r.z * r.z), 2.0f * (r.x * r.y - r.z * r.w)),
-		    _mm_set_ps(0.0f, 1.0f - 2.0f * (r.x * r.x + r.y * r.y), 2.0f * (r.y * r.z - r.x * r.w), 2.0f * (r.x * r.z + r.y * r.w)),
-		    _mm_set_ps(1.0f, 0.0f, 0.0f, 0.0f)
-
+			_mm_set_ps(0.0f, 2.0f * (r.x * r.z - r.y * r.w), 2.0f * (r.x * r.y + r.z * r.w),
+				1.0f - 2.0f * (r.y * r.y + r.z * r.z)),
+			_mm_set_ps(0.0f, 2.0f * (r.y * r.z + r.x * r.w), 1.0f - 2.0f * (r.x * r.x + r.z * r.z),
+				2.0f * (r.x * r.y - r.z * r.w)),
+			_mm_set_ps(0.0f, 1.0f - 2.0f * (r.x * r.x + r.y * r.y), 2.0f * (r.y * r.z - r.x * r.w),
+				2.0f * (r.x * r.z + r.y * r.w)),
+			_mm_set_ps(1.0f, 0.0f, 0.0f, 0.0f)
 		};
-		const mat4 translation = {_mm_set_ps(0.0f, 0.0f, 0.0f, 1.0f), _mm_set_ps(0.0f, 0.0f, 1.0f, 0.0f), _mm_set_ps(0.0f, 1.0f, 0.0f, 0.0f),
-					  _mm_set_ps(1.0f, t.z, t.y, t.x)};
-		const mat4 scale = mat4(s);
+
+		const mat4 translation = {_mm_set_ps(0.0f, 0.0f, 0.0f, 1.0f), _mm_set_ps(0.0f, 0.0f, 1.0f, 0.0f),
+			_mm_set_ps(0.0f, 1.0f, 0.0f, 0.0f), _mm_set_ps(1.0f, t.z, t.y, t.x)};
+
+		const auto scale = mat4(s);
 
 		return scale * rotation * translation;
 	}
@@ -46,7 +50,7 @@ namespace clz::math
 		const float a = far / (near - far);
 		const float b = (far * near) / (near - far);
 
-		return {_mm_set_ps(0.0f, 0.0f, 0.0f, sx), _mm_set_ps(0.0f, 0.0f, sy, 0.0f), _mm_set_ps(-1.0f, a, 0.0f, 0.0f),
-			_mm_set_ps(0.0f, b, 0.0f, 0.0f)};
+		return {_mm_set_ps(0.0f, 0.0f, 0.0f, sx), _mm_set_ps(0.0f, 0.0f, sy, 0.0f),
+			_mm_set_ps(-1.0f, a, 0.0f, 0.0f), _mm_set_ps(0.0f, b, 0.0f, 0.0f)};
 	}
 } // namespace clz::math
